7204216be5a93ea6b2a9ade9ba19709aa99983cf
Piotr Przybyla <259286@student.pwr.edu.pl>
2022-12-17 12:41:35 +0100

    Producer 0 work well

--- producers.h
+++ producers.h
@@ -16,44 +16,65 @@ void oneWord0(char *dictionaryFileName, char *passwordsFileName){
   char *line = malloc(256);
 
 //check without prefixes
-    while (fgets(line, sizeof(line), dictionaryFile))
-  {
-    
-	checkPassword(line, passwordsFileName);
+  while (fgets(line, sizeof(line), dictionaryFile))
+  { 
+	  checkPassword(line, passwordsFileName);
 	}
 
 
 //check with numbers
   int counter = 0;
-    while(1){
-    FILE *dictionaryFile = fopen(dictionaryFileName, "r");
+  while(1){
+  FILE *dictionaryFile = fopen(dictionaryFileName, "r");
   while (fgets(line, sizeof(line), dictionaryFile))
   {
-    char *newLine = malloc(256);
-    char *str = malloc(1);
-    newLine = line;
-    sprintf(str, "%d", counter);
-     //prefix
-  
-   strcat( str, line);
-	checkPassword(str, passwordsFileName);
-free(str);
+    //prefix
+    char *prefix = malloc(256);
+    char *postfix = malloc(256);
+    sprintf(prefix, "%d", counter);
+    strcat( prefix, line);
+	  checkPassword(prefix, passwordsFileName);
+    free(prefix);
+    free(postfix);
 
-
-    //postfix
-    str = malloc(1);
-    sprintf(str, "%d", counter);
-    strcat(line, str);
-    
-    checkPassword(line, passwordsFileName);
-  free(str);
-	}
-     counter++;
-     
-       
+    //prefix with all less postfixes
+    for(int i = 0; i<=counter; i++){
+    prefix = malloc(256);
+    postfix = malloc(256);
+    sprintf(prefix, "%d", counter);
+    sprintf(postfix, "%d", i);
+    strcat( prefix, line);
+    strcat(prefix, postfix);
+	  checkPassword(prefix, passwordsFileName);
+    free(prefix);
+    free(postfix);
     }
 
+    //postfix
+    prefix = malloc(256);
+    postfix = malloc(256);
+    sprintf(postfix, "%d", counter);
+    strcat(line, postfix);
+    checkPassword(line, passwordsFileName);
+    free(postfix);
+
+    //postfix with all less prefixes
+    for(int i = 0; i<=counter; i++){
+    prefix = malloc(256);
+    postfix = malloc(256);
+    sprintf(postfix, "%d", counter);
+    sprintf(prefix, "%d", i);
+    strcat(prefix, line);
+	  checkPassword(prefix, passwordsFileName);
+    free(prefix);
+    free(postfix);
+    }
+	}
+
+  counter++;
   }
+
+}
 
   // Close the file
   fclose(dictionaryFile);
